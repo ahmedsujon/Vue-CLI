fcce5ea63dbb2e273c54269add8ee2d2b95d00c6
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-20 17:36:15 +0600

    Nested Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <h2>{{ title }}</h2>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 export default {
   name: "App",
   data() {
     return {
       title: "Vue Basic Runing :)"
     };
+  },
+  components: {
+    Navbar
   }
 };
 </script>

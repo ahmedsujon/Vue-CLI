c4dcfd5afa577533a49a30f864671f6706383b60
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-20 17:15:24 +0600

    Data Function

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ title }}</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      title: "Vue Basic Runing :)"
+    };
   }
-}
+};
 </script>
 
 <style>

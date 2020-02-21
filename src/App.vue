d4073e0d4ff624d27917fde4bcf72b48c7b20c7e
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-21 08:34:23 +0600

    Axios Request

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,57 @@
 <template>
-  <div id="app">
-    <h2>{{ title }}</h2>
-    <navbar></navbar>
-    <Allfriends :friends="friends" @delete="deleteFriend"></Allfriends>
-    <Onlinefriend :friends="friends"></Onlinefriend>
-    <Hook></Hook>
-  </div>
+	<div id="app">
+		<h2>{{ title }}</h2>
+		<navbar></navbar>
+		<Allfriends :friends="friends" @delete="deleteFriend"></Allfriends>
+		<Onlinefriend :friends="friends"></Onlinefriend>
+		<Hook></Hook>
+	</div>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-import Allfriends from "./components/Allfriends.vue";
-import Onlinefriend from "./components/Onlinefriend.vue";
-import Hook from "./components/Hook.vue";
+import Navbar from './components/Navbar.vue';
+import Allfriends from './components/Allfriends.vue';
+import Onlinefriend from './components/Onlinefriend.vue';
+import Hook from './components/Hook.vue';
 
 export default {
-  name: "App",
-  data() {
-    return {
-      title: "Vue Basic Runing :)",
-      friends: [
-        { name: "A", online: true },
-        { name: "B", online: true },
-        { name: "C", online: true },
-        { name: "D", online: false },
-        { name: "E", online: true },
-        { name: "F", online: false }
-      ]
-    };
-  },
-  components: {
-    Navbar,
-    Allfriends,
-    Onlinefriend,
-    Hook
-  },
-  methods: {
-    deleteFriend(payload) {
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      });
-    }
-  }
+	name: 'App',
+	data() {
+		return {
+			title: 'Vue Basic Runing :)',
+			friends: [
+				{ name: 'A', online: true },
+				{ name: 'B', online: true },
+				{ name: 'C', online: true },
+				{ name: 'D', online: false },
+				{ name: 'E', online: true },
+				{ name: 'F', online: false }
+			]
+		};
+	},
+	components: {
+		Navbar,
+		Allfriends,
+		Onlinefriend,
+		Hook
+	},
+	methods: {
+		deleteFriend(payload) {
+			this.friends = this.friends.filter(friend => {
+				return friend.name !== payload.name;
+			});
+		}
+	}
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/components/Hook.vue
+++ src/components/Hook.vue
@@ -1,21 +1,41 @@
 <template>
-  <h1>Hook</h1>
+	<div class="Hook">
+		<h1>All Posts</h1>
+		<div v-for="post in posts" :key="post.id">
+			<h3>{{ post.id }}</h3>
+			<h3>{{ post.title }}</h3>
+			<p>{{ post.body }}</p>
+		</div>
+	</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: "Hook",
-  data() {
-    return {};
-  },
-  beforeCreate() {
-    alert("Before Created Hook");
-  },
-  mounted() {
-    alert("Created Hook");
-  }
+	name: 'Hook',
+	data() {
+		return {
+			posts: []
+		};
+	},
+	methods: {},
+	created() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts')
+			.then(response => {
+				//console.log(response);
+				this.posts = response.data;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 };
 </script>
 
-<style scoped>
+<style>
+h1 {
+	color: gray;
+	text-align: center;
+}
 </style>

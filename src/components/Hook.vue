ac19ae1eff8aeafba42db4abc014bc4395fe054d
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-21 09:15:08 +0600

    Filter

--- src/components/Hook.vue
+++ src/components/Hook.vue
@@ -1,41 +1,41 @@
 <template>
-	<div class="Hook">
-		<h1>All Posts</h1>
-		<div v-for="post in posts" :key="post.id">
-			<h3>{{ post.id }}</h3>
-			<h3>{{ post.title }}</h3>
-			<p>{{ post.body }}</p>
-		</div>
-	</div>
+  <div class="Hook">
+    <h1>All Posts</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.id }}</h3>
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body | snippet }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-	name: 'Hook',
-	data() {
-		return {
-			posts: []
-		};
-	},
-	methods: {},
-	created() {
-		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
-			.then(response => {
-				//console.log(response);
-				this.posts = response.data;
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}
+  name: "Hook",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {},
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        //console.log(response);
+        this.posts = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 </script>
 
 <style>
 h1 {
-	color: gray;
-	text-align: center;
+  color: gray;
+  text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+//Globally Filter
+Vue.filter('snippet', val => {
+    if (!val || typeof (val) != 'string') return ''
+    val = val.slice(0, 40)
+    return val
+})
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
